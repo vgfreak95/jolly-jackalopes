fa11c5b8d9dcbc53eef0bf0ed9c3966d6911367c
Richard Schroeders <richard.schroeders@broadlume.com>
2022-07-25 15:18:47 -0400

    Adding server and commands

---
+++ src/commands.py
@@ -0,0 +1,25 @@
+"""
+
+"""
+
+import click
+import uvicorn
+
+# When the is ran as script accept args
+@click.command()
+@click.option("--name", prompt="Please enter your name", help="Enters your name")
+def say_name(name: str):
+    """This will just give the users name back"""
+    click.echo(f"Welcome {name}")
+
+@click.group()
+def cli():
+    ...
+
+@cli.command()
+@click.option("--host", help="The host to run the server on")
+@click.option("--port", help="This is your port")
+@click.option("--uses_https", help="This is your host name")
+@click.option("--dev_mode", help="This is your host name")
+def serve(host: str, port: int, uses_https: bool, dev_mode: bool):
+    uvicorn.run("server:app", port=port, log_level="info")
---
+++ src/server.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            let ws = new WebSocket("ws://localhost:8080/ws");
+            ws.onmessage = function(event) {
+                let messages = document.getElementById('messages')
+                let message = document.createElement('li')
+                let content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+
+            const sendMessage = (event) => {
+                let input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
